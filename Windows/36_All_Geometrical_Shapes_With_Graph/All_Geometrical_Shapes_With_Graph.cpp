99a4f67ea77086ab7f09788d1855c47ece12dffd
Chaitanya142 <32837596+Chaitanya142@users.noreply.github.com>
2019-03-07 23:56:46 +0530

    36 37 38 39 40

---
+++ Windows/36_All_Geometrical_Shapes_With_Graph/All_Geometrical_Shapes_With_Graph.cpp
@@ -0,0 +1,454 @@
+#include<Windows.h>
+#include<stdio.h>
+#include<gl\GL.h>
+#include<gl\GLU.h>
+#define _USE_MATH_DEFINES 1
+#include<math.h>
+
+//#include <time.h>
+
+#pragma comment(lib,"OpenGL32.lib")
+#pragma comment(lib,"glu32.lib")
+
+
+//Constants
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+//Global Variables
+HWND ghwnd = NULL;
+DWORD dwStyle;
+bool bFullScreen = false;
+WINDOWPLACEMENT wpPrev = { sizeof(WINDOWPLACEMENT) };
+HDC ghdc = NULL;
+HGLRC ghrc = NULL;
+bool gbActiveWindow = false;
+FILE *gpFile = NULL;
+//Method Declaration
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+//void PrintTime();
+//WinMain Function
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevinstance, LPSTR lpszCmdLine, int iCmdShow) {
+	//Method Declaration
+	int Initialize(void);
+	void Display(void);
+	//Variable Declaration
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("OGL Window");
+	int iRet = 0;
+	bool bDone = false;
+	//Code
+	if (fopen_s(&gpFile, "Log.txt", "w") != 0)
+	{
+		MessageBox(NULL, TEXT("Log File Can't Be Created"), TEXT("ERROR"), MB_OK);
+		exit(0);
+	}
+	else {
+		//PrintTime();
+		fprintf_s(gpFile, "Log File Created Successfully\n");
+	}
+	wndclass.cbClsExtra = 0;
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.cbWndExtra = 0;
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hInstance = hInstance;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+
+	RegisterClassEx(&wndclass);
+
+	hwnd = CreateWindowEx(WS_EX_APPWINDOW,
+		szAppName,
+		TEXT("OGL WINDOW"),
+		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE,
+		100,
+		100,
+		WIN_WIDTH,
+		WIN_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	ghwnd = hwnd;
+	iRet = Initialize();
+	if (iRet == -1) {
+		fprintf_s(gpFile, "Choose Pixel Format Failed\n");
+		DestroyWindow(0);
+	}
+	else if (iRet == -2) {
+		fprintf_s(gpFile, "Set Pixel Format Failed\n");
+		DestroyWindow(0);
+	}
+	else if (iRet == -3) {
+		fprintf_s(gpFile, "wgl Create Context Failed\n");
+		DestroyWindow(0);
+	}
+	else if (iRet == -4) {
+		fprintf_s(gpFile, "wgl Make Current Failed\n");
+		DestroyWindow(0);
+	}
+	else {
+		fprintf_s(gpFile, "Initialization Succeeded\n");
+	}
+	ShowWindow(hwnd, iCmdShow);
+	//Do not call update window
+	SetForegroundWindow(hwnd);
+	SetFocus(hwnd);
+	//Game Loop
+	//SendMessage(hwnd, WM_PAINT, 0, 0);
+	while (bDone == false) {
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+			{
+				bDone = true;
+			}
+			else {
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else {
+			if (gbActiveWindow == true) {
+				//Here Call Update
+			}
+			Display();
+			//Here Call Display
+		}
+	}
+	return (int)msg.wParam;
+}
+//WndProc Function
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam) {
+	//Function Declaration
+	void ToggleFullScreen(void);
+	void Resize(int, int);
+	void UnInitialize(void);
+	//Code
+	switch (iMsg)
+	{
+	case WM_SETFOCUS:
+		gbActiveWindow = true;
+		break;
+	case WM_KILLFOCUS:
+		gbActiveWindow = false;
+		break;
+	case WM_SIZE:
+		Resize(LOWORD(lParam), HIWORD(lParam));
+		break;
+	case WM_ERASEBKGND:
+		return(0);
+		break;
+	case WM_CLOSE:
+		DestroyWindow(0);
+		break;
+	case WM_CHAR:
+		switch (wParam)
+		{
+		case 'F':
+		case 'f':
+			ToggleFullScreen();
+			break;
+		case VK_ESCAPE:
+			DestroyWindow(0);
+			break;
+		}
+		break;
+	case WM_DESTROY:
+		UnInitialize();
+		PostQuitMessage(0);
+		break;
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+//ToggleFullScreen Function
+void ToggleFullScreen(void) {
+	//Variable Declaration
+	MONITORINFO mi;
+	if (bFullScreen == false) {
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+		if (dwStyle & WS_OVERLAPPEDWINDOW) {
+			mi = { sizeof(MONITORINFO) };
+			if (GetWindowPlacement(ghwnd, &wpPrev) && GetMonitorInfo(MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY), &mi)) {
+				SetWindowLong(ghwnd, GWL_STYLE, dwStyle & ~WS_OVERLAPPEDWINDOW);
+				SetWindowPos(ghwnd,
+					HWND_TOP,
+					mi.rcMonitor.left,
+					mi.rcMonitor.top,
+					mi.rcMonitor.right - mi.rcMonitor.left,
+					mi.rcMonitor.bottom - mi.rcMonitor.top,
+					SWP_NOZORDER | SWP_FRAMECHANGED
+				);
+				ShowCursor(FALSE);
+				bFullScreen = true;
+			}
+		}
+	}
+	else {
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPlacement(ghwnd, &wpPrev);
+		SetWindowPos(ghwnd,
+			HWND_TOP,
+			0,
+			0,
+			0,
+			0,
+			SWP_NOZORDER | SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER);
+		ShowCursor(TRUE);
+		bFullScreen = false;
+	}
+}
+//Initialize OpenGl
+int Initialize(void) {
+	//Function Declaration
+	void Resize(int, int);
+	PIXELFORMATDESCRIPTOR pfd;
+	int iPixelFormatIndex;
+	//Code
+	ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW |
+		PFD_SUPPORT_OPENGL |
+		PFD_DOUBLEBUFFER;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cRedBits = 8;
+	pfd.cGreenBits = 8;
+	pfd.cBlueBits = 8;
+	pfd.cAlphaBits = 8;
+	ghdc = GetDC(ghwnd);
+	iPixelFormatIndex = ChoosePixelFormat(ghdc, &pfd);
+	if (iPixelFormatIndex == 0) {
+		return -1;
+	}
+	if (SetPixelFormat(ghdc, iPixelFormatIndex, &pfd) == FALSE)
+	{
+		return -2;
+	}
+	ghrc = wglCreateContext(ghdc);
+	if (ghrc == NULL) {
+		return -3;
+	}
+	if (wglMakeCurrent(ghdc, ghrc) == FALSE) {
+		return -4;
+	}
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	Resize(WIN_WIDTH, WIN_HEIGHT);
+	return 0;
+}
+//Function Resize
+void Resize(int width, int height) {
+	if (height == 0)
+		height = 1;
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f,
+		(GLfloat)width / (GLfloat)height,
+		0.1f,
+		100.0f);
+
+}
+//Function Display
+void Display(void) {
+
+	void DrawTriangle(float);
+	void DrawInCircle(float x1, float y1, float z1,
+		float x2, float y2, float z2,
+		float x3, float y3, float z3
+	);
+	void DrawGraph(GLfloat offset);
+	void DrawCircle(GLfloat radius);
+	void DrawRectangle(GLfloat width, GLfloat height);
+	void DrawCircleAroundRectangle(GLfloat width, GLfloat height);
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glTranslatef(0.0f, 0.0f, -3.0f);
+
+	DrawGraph(0.01f);
+	DrawTriangle(0.5f);
+	DrawInCircle(0.0f, 0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f
+	);
+	DrawRectangle(1.0f,1.0f);
+	DrawCircleAroundRectangle(1.0f, 1.0f);
+
+	SwapBuffers(ghdc);
+}
+void UnInitialize(void) {
+	if (bFullScreen == true) {
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPlacement(ghwnd, &wpPrev);
+		SetWindowPos(ghwnd,
+			HWND_TOP,
+			0,
+			0,
+			0,
+			0,
+			SWP_NOZORDER | SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER);
+		//ShowCursor(TRUE);
+	}
+	if (wglGetCurrentContext() == ghrc) {
+		wglMakeCurrent(NULL, NULL);
+	}
+	if (ghrc) {
+		wglDeleteContext(ghrc);
+		ghrc = NULL;
+	}
+	if (ghdc) {
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+	if (gpFile) {
+		fprintf_s(gpFile, "Log File Closed Successfully");
+		fclose(gpFile);
+		gpFile = NULL;
+	}
+}
+
+void DrawTriangle(float offset)
+{
+	float count = -1;
+	int noCount = 2000;
+	GLfloat angle = 0;
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 1.0f, 0.0f);
+	
+	glVertex3f(0.0f, offset, 0.0f);
+	glVertex3f(-offset, -offset, 0.0f);
+
+	glVertex3f(-offset, -offset, 0.0f);
+	glVertex3f(offset, -offset, 0.0f);
+
+	glVertex3f(offset, -offset, 0.0f);
+	glVertex3f(0.0f, offset, 0.0f);
+
+	glEnd();
+}
+
+void DrawInCircle(float x1, float y1, float z1,
+	float x2, float y2, float z2,
+	float x3, float y3, float z3
+)
+{
+	float count = -1;
+	int noCount = 2000;
+	GLfloat angle = 0;
+
+	float distP1P2 = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) + (z2 - z1)*(z2 - z1));
+	float distP2P3 = sqrt((x3 - x2)*(x3 - x2) + (y3 - y2)*(y3 - y2) + (z3 - z2)*(z3 - z2));
+	float distP3P1 = sqrt((x1 - x3)*(x1 - x3) + (y1 - y3)*(y1 - y3) + (z1 - z3)*(z1 - z3));
+
+	float semiPerimeter = (distP1P2 + distP2P3 + distP3P1) / 2;
+
+	float radius = sqrt((semiPerimeter - distP1P2)*(semiPerimeter - distP2P3)*(semiPerimeter - distP3P1) / semiPerimeter);
+
+	float Ox = (x3 * distP1P2 + x1 * distP2P3 + x2 * distP3P1) / (semiPerimeter * 2);
+	float Oy = (y3 * distP1P2 + y1 * distP2P3 + y2 * distP3P1) / (semiPerimeter * 2);
+	float Oz = (z3 * distP1P2 + z1 * distP2P3 + z2 * distP3P1) / (semiPerimeter * 2);
+
+	glBegin(GL_LINE_LOOP);
+	glColor3f(1.0f,1.0f,0.0f);
+	for (count = 0; count <= noCount; count++) {
+		angle = 2 * M_PI*count / noCount;
+		glVertex3f(cos(angle)*radius + Ox, sin(angle)*radius + Oy, 0.0f + Oz);
+	}
+
+	glEnd();
+
+}
+
+void DrawGraph(GLfloat offset) {
+
+	glBegin(GL_LINES);
+
+	glColor3f(0.0, 0.0f, 1.0f);
+		for (GLfloat count = -1.00f; count <= 1.00f; count = count + offset)
+		{
+			glVertex3f(count,-1.0f,0.0f);
+			glVertex3f(count, 1.0f, 0.0f);
+
+			glVertex3f(-1.0f, count,  0.0f);
+			glVertex3f(1.0f, count, 0.0f);
+
+		}
+		
+	glEnd();
+
+}
+
+void DrawCircle(GLfloat radius) {
+	float count = -1;
+	int noCount = 2000;
+	GLfloat angle = 0;
+
+	glBegin(GL_LINE_LOOP);
+	glColor3f(1.0f,1.0f,0.0f);
+	for (count = 0; count <= noCount; count++) {
+		angle = 2 * M_PI*count / noCount;
+		glVertex3f(cos(angle)*radius , sin(angle)*radius, 0.0f );
+	}
+
+	glEnd();
+
+}
+
+void DrawRectangle(GLfloat width, GLfloat height) {
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 1.0f, 0.0f);
+
+	glVertex3f(width/2, height/2, 0.0f);
+	glVertex3f(-width / 2, height / 2, 0.0f);
+
+	glVertex3f(-width / 2, height / 2, 0.0f);
+	glVertex3f(-width / 2, -height / 2, 0.0f);
+
+	glVertex3f(-width / 2, -height / 2, 0.0f);
+	glVertex3f(width / 2, -height / 2, 0.0f);
+
+	glVertex3f(width / 2, -height / 2, 0.0f);
+	glVertex3f(width / 2, height / 2, 0.0f);
+
+	glEnd();
+}
+
+void DrawCircleAroundRectangle(GLfloat width, GLfloat height) {
+	float count = -1;
+	int noCount = 2000;
+	GLfloat angle = 0;
+	float radius = sqrt(width / 2 * width / 2 + height / 2 * height / 2);
+
+	glBegin(GL_LINE_LOOP);
+	glColor3f(1.0f, 1.0f, 0.0f);
+	for (count = 0; count <= noCount; count++) {
+		angle = 2 * M_PI*count / noCount;
+		glVertex3f(cos(angle)*radius, sin(angle)*radius, 0.0f);
+	}
+
+	glEnd();
+}
+
+
+//void PrintTime() {
+//	struct tm newtime;
+//	__time64_t long_time;
+//	errno_t err;
+//	_time64(&long_time);// Get time as 64-bit integer.
+//	err = _localtime64_s(&newtime, &long_time);// Convert to local time.
+//	if (!err && gpFile) {
+//		fprintf_s(gpFile, "%i:%i:%i %i-%i-%i ",newtime.tm_mday,newtime.tm_mon+1,newtime.tm_year+1900,newtime.tm_hour,newtime.tm_min,newtime.tm_sec);
+//	}
+//}
