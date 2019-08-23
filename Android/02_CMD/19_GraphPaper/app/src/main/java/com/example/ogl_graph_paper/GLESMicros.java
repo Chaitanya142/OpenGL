38dc5c3598a7e92ff4f91703ff4ef92e9b8e0252
Chaitanya142 <32837596+Chaitanya142@users.noreply.github.com>
2019-08-23 20:00:30 +0530

    Android PP

---
+++ Android/02_CMD/19_GraphPaper/app/src/main/java/com/example/ogl_graph_paper/GLESMicros.java
@@ -0,0 +1,10 @@
+package com.example.ogl_graph_paper;
+
+public class GLESMicros{
+
+    public static final int AMC_ATTRIBUTE_POSITION = 0;
+	public static final int AMC_ATTRIBUTE_COLOR = 1;
+	public static final int AMC_ATTRIBUTE_NORMAL = 2;
+	public static final int AMC_ATTRIBUTE_TEXCOORD0 = 3;
+
+}
